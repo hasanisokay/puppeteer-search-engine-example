38d94964d2d8629824d885f9eff859b116981513
hasanisokay <hasanisokay@gmail.com>
2023-09-11 12:40:57 +0600

    added search text

--- get-search.mjs
+++ get-search.mjs
@@ -1,7 +1,13 @@
 import puppeteer from "puppeteer";
-const browser = await puppeteer.launch()
+const browser = await puppeteer.launch({
+    headless: true
+})
 const page = await browser.newPage();
 await page.goto("https://duckduckgo.com");
+await page.waitForSelector("#searchbox_input")
+await page.type("#searchbox_input", "hasan")
+await page.click(`[aria-label='Search']`)
+await page.waitForSelector('.react-results--main')
 await page.screenshot({path:"duckduckgo.com.png"});
 await page.close();
 await browser.close();
