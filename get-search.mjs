70f1142e15d374a9c07d861a3d92f0993c7e8f06
hasanisokay <hasanisokay@gmail.com>
2024-06-01 11:41:36 +0600

    added puppeteer script

--- get-search.mjs
+++ get-search.mjs
@@ -1,13 +1,18 @@
 import puppeteer from "puppeteer";
 const browser = await puppeteer.launch({
-    headless: true
-})
+  headless: false,
+});
 const page = await browser.newPage();
 await page.goto("https://duckduckgo.com");
-await page.waitForSelector("#searchbox_input")
-await page.type("#searchbox_input", "hasan")
-await page.click(`[aria-label='Search']`)
-await page.waitForSelector('.react-results--main')
-await page.screenshot({path:"duckduckgo.com.png"});
+await page.waitForSelector("#searchbox_input");
+await page.type("#searchbox_input", "hasan");
+await page.click(`[aria-label='Search']`);
+await page.waitForSelector(".react-results--main");
+await page.screenshot({ path: "duckduckgo.com.png" });
+const resultsHeading = await page.evaluate(() => {
+  let items = Array.from(document.querySelectorAll("h2"));
+  return items.map((item) => item.innerText);
+});
+console.log(resultsHeading)
 await page.close();
 await browser.close();
